5221eb8dff0baff2650f0f3ed06f5637b225d9bd
Ata <atakanozalan@gmail.com>
2024-04-09 21:27:31 +0300

    Initial Commit

---
+++ cekilis/cekilisApp.swift
@@ -0,0 +1,17 @@
+//
+//  cekilisApp.swift
+//  cekilis
+//
+//  Created by Ezagor on 9.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct cekilisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
